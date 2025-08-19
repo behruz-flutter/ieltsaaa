76190dabf2bde137c2ded74af25e85b24f8bbd30
behruz-flutter <behruzdilmurodov550@gmail.com>
2025-08-19 14:49:02 +0500

    first commit

---
+++ stozzziki/main.js
@@ -0,0 +1,10 @@
+// let input = document.querySelector('input')
+// let btn = document.querySelector('button')
+// let img = document.querySelector('img')
+
+// btn.addEventListener('click',()=>{
+//     img.setAttribute('src',input.value)
+// }
+// )
+
+
